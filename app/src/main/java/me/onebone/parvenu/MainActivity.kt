76dc8fe3b57946d9999f5b75c4d56a800ddf5a49
onebone <jyc00410@gmail.com>
2022-05-05 00:07:16 +0900

    reindent code

--- app/src/main/java/me/onebone/parvenu/MainActivity.kt
+++ app/src/main/java/me/onebone/parvenu/MainActivity.kt
@@ -24,125 +24,125 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
 
-        setContent {
-            var editorValue by remember { mutableStateOf(
-                ParvenuEditorValue(
-                    parvenuString = ParvenuAnnotatedString(
-                        text = "normal bold",
-                        spanStyles = listOf(
-                            ParvenuAnnotatedString.Range(
-                                item = SpanStyle(fontWeight = FontWeight.Bold),
-                                start = 7, end = 11,
-                                startInclusive = false,
-                                endInclusive = true
-                            )
-                        )
-                    ),
-                    selection = TextRange.Zero,
-                    composition = null
-                )
-            ) }
+		setContent {
+			var editorValue by remember { mutableStateOf(
+				ParvenuEditorValue(
+					parvenuString = ParvenuAnnotatedString(
+						text = "normal bold",
+						spanStyles = listOf(
+							ParvenuAnnotatedString.Range(
+								item = SpanStyle(fontWeight = FontWeight.Bold),
+								start = 7, end = 11,
+								startInclusive = false,
+								endInclusive = true
+							)
+						)
+					),
+					selection = TextRange.Zero,
+					composition = null
+				)
+			) }
 
-            val italic = SpanStyle(fontStyle = FontStyle.Italic)
+			val italic = SpanStyle(fontStyle = FontStyle.Italic)
 
-            Column(modifier = Modifier.fillMaxSize()) {
-                Button(
-                    onClick = {
-                        val selection = editorValue.selection
+			Column(modifier = Modifier.fillMaxSize()) {
+				Button(
+					onClick = {
+						val selection = editorValue.selection
 
-                        val fillRanges = editorValue.parvenuString.spanStyles.fillsRange(
-                            selection.start, selection.end
-                        ) {
-                            it.fontStyle == FontStyle.Italic
-                        }
+						val fillRanges = editorValue.parvenuString.spanStyles.fillsRange(
+							selection.start, selection.end
+						) {
+							it.fontStyle == FontStyle.Italic
+						}
 
-                        println(editorValue.parvenuString.spanStyles.joinToString("\n") {
-                            "${it.start}..${it.end} => style=${it.item.fontStyle}, weight=${it.item.fontWeight}"
-                        })
-                        println("fillRanges=$fillRanges")
-                        println()
+						println(editorValue.parvenuString.spanStyles.joinToString("\n") {
+							"${it.start}..${it.end} => style=${it.item.fontStyle}, weight=${it.item.fontWeight}"
+						})
+						println("fillRanges=$fillRanges")
+						println()
 
-                        if (
-                            !fillRanges
-                        ) {
-                            editorValue = ParvenuEditorValue(
-                                parvenuString = ParvenuAnnotatedString(
-                                    text = editorValue.parvenuString.text,
-                                    spanStyles = editorValue.parvenuString.spanStyles + ParvenuAnnotatedString.Range(
-                                        item = italic,
-                                        start = selection.start, end = selection.end,
-                                        startInclusive = false, endInclusive = true
-                                    )
-                                ),
-                                selection = editorValue.selection,
-                                composition = editorValue.composition
-                            )
-                        } else {
-                            val nonEmptyPredicate: (ParvenuAnnotatedString.Range<*>) -> Boolean = {
-                                it.start != it.end
-                            }
+						if (
+							!fillRanges
+						) {
+							editorValue = ParvenuEditorValue(
+								parvenuString = ParvenuAnnotatedString(
+									text = editorValue.parvenuString.text,
+									spanStyles = editorValue.parvenuString.spanStyles + ParvenuAnnotatedString.Range(
+										item = italic,
+										start = selection.start, end = selection.end,
+										startInclusive = false, endInclusive = true
+									)
+								),
+								selection = editorValue.selection,
+								composition = editorValue.composition
+							)
+						} else {
+							val nonEmptyPredicate: (ParvenuAnnotatedString.Range<*>) -> Boolean = {
+								it.start != it.end
+							}
 
-                            editorValue = ParvenuEditorValue(
-                                parvenuString = ParvenuAnnotatedString(
-                                    text = editorValue.parvenuString.text,
-                                    spanStyles = editorValue.parvenuString.spanStyles.flatMap { range ->
-                                        if (range.item.fontStyle != FontStyle.Italic) return@flatMap listOf(range)
+							editorValue = ParvenuEditorValue(
+								parvenuString = ParvenuAnnotatedString(
+									text = editorValue.parvenuString.text,
+									spanStyles = editorValue.parvenuString.spanStyles.flatMap { range ->
+										if (range.item.fontStyle != FontStyle.Italic) return@flatMap listOf(range)
 
-                                        if (selection.start <= range.start && range.end <= selection.end) {
-                                            emptyList()
-                                        } else if (range.start <= selection.start && selection.end <= range.end) {
-                                            // SELECTION:      -----
-                                            // RANGE    :    ----------
-                                            // REMAINDER:    __     ___
-                                            listOf(
-                                                range.copy(end = selection.start),
-                                                range.copy(start = selection.end)
-                                            ).filter(nonEmptyPredicate)
-                                        } else if (selection.start in range) {
-                                            // SELECTION:     ---------
-                                            // RANGE    : --------
-                                            // REMAINDER: ____
-                                            listOf(range.copy(end = selection.start))
-                                                .filter(nonEmptyPredicate)
-                                        } else if (selection.end in range) {
-                                            // SELECTION: ---------
-                                            // RANGE    :      --------
-                                            // REMAINDER:          ____
-                                            listOf(range.copy(start = selection.end, end = range.end))
-                                                .filter(nonEmptyPredicate)
-                                        } else {
-                                            listOf(range)
-                                        }
-                                    }
-                                ),
-                                selection = selection,
-                                composition = editorValue.composition
-                            )
-                        }
-                    }
-                ) {
-                    Text("italic")
-                }
+										if (selection.start <= range.start && range.end <= selection.end) {
+											emptyList()
+										} else if (range.start <= selection.start && selection.end <= range.end) {
+											// SELECTION:      -----
+											// RANGE    :    ----------
+											// REMAINDER:    __     ___
+											listOf(
+												range.copy(end = selection.start),
+												range.copy(start = selection.end)
+											).filter(nonEmptyPredicate)
+										} else if (selection.start in range) {
+											// SELECTION:     ---------
+											// RANGE    : --------
+											// REMAINDER: ____
+											listOf(range.copy(end = selection.start))
+												.filter(nonEmptyPredicate)
+										} else if (selection.end in range) {
+											// SELECTION: ---------
+											// RANGE    :      --------
+											// REMAINDER:          ____
+											listOf(range.copy(start = selection.end, end = range.end))
+												.filter(nonEmptyPredicate)
+										} else {
+											listOf(range)
+										}
+									}
+								),
+								selection = selection,
+								composition = editorValue.composition
+							)
+						}
+					}
+				) {
+					Text("italic")
+				}
 
-                ParvenuEditor(
-                    value = editorValue,
-                    onValueChange = {
-                        editorValue = it
-                    }
-                ) { value, onValueChange ->
-                    BasicTextField(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        value = value,
-                        onValueChange = onValueChange,
-                        textStyle = TextStyle(fontSize = 21.sp)
-                    )
-                }
-            }
-        }
-    }
+				ParvenuEditor(
+					value = editorValue,
+					onValueChange = {
+						editorValue = it
+					}
+				) { value, onValueChange ->
+					BasicTextField(
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(16.dp),
+						value = value,
+						onValueChange = onValueChange,
+						textStyle = TextStyle(fontSize = 21.sp)
+					)
+				}
+			}
+		}
+	}
 }
